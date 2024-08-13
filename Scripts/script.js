237c6bee5d62374f4d757f4be3139121c7cd87eb
João Paulo <103288654+Jounys@users.noreply.github.com>
2024-08-13 10:57:34 -0300

    Função de Copy

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,58 +1,83 @@
-let textArea = document.querySelector('#text-area')
-let mensagem = document.querySelector('#mensagem')
-let criptografar = document.querySelector('#criptografar')
-let descriptografar = document.querySelector('#descriptografar')
+let textArea = document.querySelector("#text-area");
+let mensagem = document.querySelector("#mensagem");
+let criptografar = document.querySelector("#criptografar");
+let descriptografar = document.querySelector("#descriptografar");
+let copiar = document.querySelector("#copiar");
 
-criptografar.addEventListener('click', btnCriptografar);
-descriptografar.addEventListener('click', btnDesencriptar);
+criptografar.addEventListener("click", btnCriptografar);
+descriptografar.addEventListener("click", btnDesencriptar);
+copiar.addEventListener("click", copiarParaAreaDeTransferencia);
 
 function btnCriptografar() {
     const textoEncriptado = encriptar(textArea.value);
     mensagem.innerHTML = textoEncriptado;
-    textArea.value = '';
+    textArea.value = "";
 }
 
-function encriptar(stringEncriptada){
-    let chaves = [
+function encriptar(stringEncriptada) {
+  let chaves = [
+    ["a", "ai"],
+    ["e", "enter"],
+    ["i", "imes"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringEncriptada = stringEncriptada.toLowerCase();
 
-        ['a', 'ai'],
-        ['e', 'enter'],
-        ['i', 'imes'],
-        ['o', 'ober'],
-        ['u', 'ufat']
-    ]
-    stringEncriptada = stringEncriptada.toLowerCase();
-
-    for(let i = 0; i < chaves.length; i++){
-        if(stringEncriptada.includes(chaves[i][0])){
-            stringEncriptada = stringEncriptada.replaceAll(chaves[i][0], chaves[i][1])
-        }
+  for (let i = 0; i < chaves.length; i++) {
+    if (stringEncriptada.includes(chaves[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(
+        chaves[i][0],
+        chaves[i][1]
+      );
     }
-    return stringEncriptada;
+  }
+  atulizacao();
+  return stringEncriptada;
 }
 
 function btnDesencriptar() {
-    const textoDesencriptado = desencriptar(textArea.value);
-    mensagem.innerHTML = textoDesencriptado;
-    textArea.value = '';
-    console.log(textoDesencriptado)
+  const textoDesencriptado = desencriptar(textArea.value);
+  mensagem.innerHTML = textoDesencriptado;
+  textArea.value = "";
+  console.log(textoDesencriptado);
 }
 
-function desencriptar(stringDesencriptada){
-    let chaves = [
+function desencriptar(stringDesencriptada) {
+  let chaves = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"], // Que bug é esse Alura? haha
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringDesencriptada = stringDesencriptada.toLowerCase();
 
-        ['e', 'enter'],
-        ['i', 'imes'],
-        ['a', 'ai'], // Que bug é esse Alura? haha
-        ['o', 'ober'],
-        ['u', 'ufat']
-    ]
-    stringDesencriptada = stringDesencriptada.toLowerCase();
-
-    for(let i = 0; i < chaves.length; i++){
-        if(stringDesencriptada.includes(chaves[i][1])){
-            stringDesencriptada = stringDesencriptada.replaceAll(chaves[i][1], chaves[i][0])
-        }
+  for (let i = 0; i < chaves.length; i++) {
+    if (stringDesencriptada.includes(chaves[i][1])) {
+      stringDesencriptada = stringDesencriptada.replaceAll(
+        chaves[i][1],
+        chaves[i][0]
+      );
     }
-    return stringDesencriptada;
+  }
+  return stringDesencriptada;
+}
+
+function atulizacao() {
+  let elemento = document.querySelector(".container__textoDescriptado__mensagens");
+  let container = document.querySelector(".container__textoDescriptado");
+  elemento.style.display = "none";
+  container.style.justifyContent = "unset";
+}
+
+function copiarParaAreaDeTransferencia() {
+  const texto = mensagem.innerText;
+  navigator.clipboard.writeText(texto)
+    .then(() => {
+      alert("Texto copiado para a área de transferência!");
+    })
+    .catch((err) => {
+      console.error("Falha ao copiar o texto: ", err);
+    });
 }
